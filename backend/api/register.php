989296ddd22f222e3c736d8aba2b799b08f3f955
jesusm2000 <ivanmoreiraloor2019@hotmail.com>
2025-03-12 13:31:52 -0500

    Evitar Correos duplicados y correo que no sean validos

--- backend/api/register.php
+++ backend/api/register.php
@@ -2,22 +2,45 @@
 require_once '../config/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
+    $data = json_decode(json: file_get_contents(filename: 'php://input'), associative: true);
     $email = $data['email'];
     $password = $data['password'];
 
+    // Email con formato valido
+    if (!filter_var(value: $email, filter: FILTER_VALIDATE_EMAIL)) {
+        http_response_code(response_code: 400);
+        echo json_encode(value: ['error' => 'Email inválido.']);
+        exit;
+    }
+
+    // Se verifica si el email ya esta registrado en la base de datos
+    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
+    $stmt->execute(['email' => $email]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user) {
+        http_response_code(response_code: 400);
+        echo json_encode(value: ['error' => 'El email ya está registrado.']);
+        exit;
+    }
+
     // Aqui se genera el hash de la contraseña
-    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $hash = password_hash(password: $password, algo: PASSWORD_DEFAULT);
 
-    $stmt = $conn->prepare("INSERT INTO usuarios (email, password) VALUES (:email, :password)");
-    $stmt->execute([
-        'email' => $email,
-        'password' => $hash
-    ]);
+    try {
+        $stmt = $conn->prepare("INSERT INTO usuarios (email, password) VALUES (:email, :password)");
 
-    echo json_encode(['message' => 'Usuario registrado.']);
+        $stmt->execute([
+            'email' => $email,
+            'password' => $hash
+        ]);
+        echo json_encode(value: ['message' => 'Usuario registrado.']);
+    } catch (PDOException $e) {
+        http_response_code(500); 
+        echo json_encode(['error' => 'Error al registrar el usuario.']);
+    }
 } else {
-    http_response_code(405); // Método no permitido
-    echo json_encode(['error' => 'Método no permitido.']);
+    http_response_code(response_code: 405);
+    echo json_encode(value: ['error' => 'Método no permitido.']);
 }
 ?>
